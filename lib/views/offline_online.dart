c5b3d2855ad663e244eadbeef8a4b5ac189441b8
sparsh04 <56976562+sparsh04@users.noreply.github.com>
2022-01-06 01:50:26 +0530

    Update offline_online.dart

--- lib/views/offline_online.dart
+++ lib/views/offline_online.dart
@@ -1,11 +1,14 @@
 import 'dart:ffi';
 
+import 'package:chattingapp/views/chatscreen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class allChats extends StatefulWidget {
-  const allChats({Key? key}) : super(key: key);
+  //const allChats({Key? key}) : super(key: key);
+  String lastmessage, myusername, username;
+  allChats(this.lastmessage, this.myusername, this.username);
 
   @override
   _allChatsState createState() => _allChatsState();
@@ -35,21 +38,26 @@ class _allChatsState extends State<allChats> {
         height: 30,
         child: StreamBuilder<QuerySnapshot>(
             builder: (context, snapshot) {
-              if(snapshot.connectionState!=ConnectionState.done){
+              if (snapshot.connectionState != ConnectionState.done) {
                 return CircularProgressIndicator();
               }
               List<String> uid = [];
               //snapshot.data!.docs.
               snapshot.data!.docs.forEach((element) {
-                Map<String,dynamic> d = element.data() as Map<String,dynamic>;
-                if(d.containsKey('uid')) {
+                Map<String, dynamic> d = element.data() as Map<String, dynamic>;
+                if (d.containsKey('uid')) {
                   uid.add(d['uid']);
                 }
               });
               print(uid);
-              return ListView.builder(itemBuilder: (context,index){
-                return statusUpdate(uid: uid[index]);
-              },
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  return statusUpdate(
+                      uid: uid[index],
+                      lastmessage: widget.lastmessage,
+                      myusername: widget.myusername,
+                      username: widget.username);
+                },
                 itemCount: uid.length,
               );
             },
@@ -64,7 +72,12 @@ class _allChatsState extends State<allChats> {
 
 class statusUpdate extends StatefulWidget {
   final String uid;
-  statusUpdate({required this.uid});
+  final String lastmessage, myusername, username;
+  statusUpdate(
+      {required this.uid,
+      required this.lastmessage,
+      required this.myusername,
+      required this.username});
 
   @override
   _statusUpdateState createState() => _statusUpdateState();
@@ -79,8 +92,58 @@ class _statusUpdateState extends State<statusUpdate> {
         stream: _firestore.collection("users").doc(widget.uid).snapshots(),
         builder: (context, snapshot) {
           if (snapshot.data != null) {
-            return Container(
-              child: Text("${snapshot.data!['uid']}"),
+            return SingleChildScrollView(
+              child: Row(
+                //child: Text("${snapshot.data!['uid']}"),
+
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              ChatScreen(widget.myusername, widget.username),
+                        ),
+                      );
+                    },
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(32),
+                      child: Image.network(
+                        snapshot.data!['imgUrl']!,
+                        height: 30,
+                        width: 30,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${snapshot.data!['name']!}",
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      // const SizedBox(
+                      //   height: 1,
+                      // ),
+                      Text(widget.lastmessage),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                    ],
+                  ),
+
+                  // Container(
+                  //   decoration: BoxDecoration(
+                  //     color: status == "Online" ? Colors.green : Colors.blue,
+                  //     borderRadius: BorderRadius.circular(1),
+                  //   ),
+                  // ),
+                ],
+              ),
             );
           } else {
             return Container();
