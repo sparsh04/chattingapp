be34ab8158dbd01225f11028bf5c9a4aee95faf5
sparsh04 <56976562+sparsh04@users.noreply.github.com>
2022-02-04 02:08:57 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-import 'package:chattingapp/helperfunctions/authenticate.dart';
-import 'package:chattingapp/services/auth.dart';
-import 'package:chattingapp/views/tabs_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import "dart:core";
+import 'package:flutter/services.dart';
+import 'dart:core';
+import 'package:my_kisan/constant.dart';
+import 'package:my_kisan/routes.dart';
+import 'package:my_kisan/screens/Login_Screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,44 +12,23 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  // bool showIgnIn = true;
-
-  // void toggleView() {
-  //   setState(() {
-  //     showIgnIn = !showIgnIn;
-  //   });
-  // }
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: FutureBuilder(
-        future: AuthMedthods().getCurrentUser(),
-        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          if (snapshot.connectionState != ConnectionState.waiting) {
-            if (snapshot.hasData) {
-              return TabsScreen(0);
-            } else {
-              return const Authenticate();
-            }
-          } else {
-            return const CircularProgressIndicator();
-          }
-        },
-      ),
+          appBarTheme: const AppBarTheme(
+              color: greencolor,
+              centerTitle: true,
+              systemOverlayStyle:
+                  SystemUiOverlayStyle(statusBarColor: greencolor))),
+      routes: routes,
+      debugShowCheckedModeBanner: false,
+      title: 'My Kisan',
+      home: LoginScreen(),
     );
   }
 }
