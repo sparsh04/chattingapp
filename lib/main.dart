287f2dd9bbe0bb1c0645faef7214bb10206418f1
sparsh04 <56976562+sparsh04@users.noreply.github.com>
2022-02-04 02:17:03 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
+import 'package:chattingapp/helperfunctions/authenticate.dart';
+import 'package:chattingapp/services/auth.dart';
+import 'package:chattingapp/views/tabs_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'dart:core';
-import 'package:my_kisan/constant.dart';
-import 'package:my_kisan/routes.dart';
-import 'package:my_kisan/screens/Login_Screen.dart';
+import "dart:core";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,23 +11,44 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // bool showIgnIn = true;
+
+  // void toggleView() {
+  //   setState(() {
+  //     showIgnIn = !showIgnIn;
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          appBarTheme: const AppBarTheme(
-              color: greencolor,
-              centerTitle: true,
-              systemOverlayStyle:
-                  SystemUiOverlayStyle(statusBarColor: greencolor))),
-      routes: routes,
+      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      title: 'My Kisan',
-      home: LoginScreen(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FutureBuilder(
+        future: AuthMedthods().getCurrentUser(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.connectionState != ConnectionState.waiting) {
+            if (snapshot.hasData) {
+              return TabsScreen(0);
+            } else {
+              return const Authenticate();
+            }
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      ),
     );
   }
 }
